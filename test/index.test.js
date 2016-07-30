eeca20d49c8077f2e8daf813df42b71e2bcb12aa
Anders D. Johnson <adjohnson916@users.noreply.github.com>
2016-07-30 18:10:35 -0500

    feat: add option to return source
    Closes #2

--- test/index.test.js
+++ test/index.test.js
@@ -2,22 +2,28 @@ import webpack from 'webpack';
 import load from '../src';
 import { join } from 'path';
 
+const config = {
+  entry: join(__dirname, './fixtures/foo.js'),
+  target: 'node',
+  output: {
+    libraryTarget: 'commonjs2',
+    filename: 'bar.js',
+    path: join(__dirname, './output'),
+  },
+  module: {
+    loaders: [{
+      test: /\.js$/,
+      loaders: ['babel'],
+    }],
+  },
+};
+
 describe('lib tests', () => {
-  it('compile to memory', () => load(webpack({
-    entry: join(__dirname, './fixtures/foo.js'),
-    target: 'node',
-    output: {
-      libraryTarget: 'commonjs2',
-      filename: 'bar.js',
-      path: join(__dirname, './output'),
-    },
-    module: {
-      loaders: [{
-        test: /\.js$/,
-        loaders: ['babel'],
-      }],
-    },
-  })).then(files => expect(files).to.deep.equal({
+  it('compile to memory', () => load(webpack(config))
+  .then(files => expect(files).to.deep.equal({
     'bar.js': 'foo',
   })));
+
+  it('compile source to memory', () => load(webpack(config))
+  .then(files => expect(files['bar.js']).to.be.a('string')));
 });
